4afa16c7f85e23b6bde5efae32c996b8502393df
thelonius182 <lcavdakker@gmail.com>
2020-01-30 23:39:38 +0100

    initial commit

--- src/stage_host_reports.R
+++ src/stage_host_reports.R
@@ -1,4 +1,4 @@
-iTunes_Playlist_Scanner_UitzendMac_30_01_20 <-
+iTunes_Playlist_Scanner_UitzendMac <-
   read_delim(
     "//uitzendmac-cz/tech/Documents/Salsa/IPS-output/iTunes Playlist Scanner (UitzendMac), 30-01-20.txt",
     "\t",
