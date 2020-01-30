11c7a9cf2e7349074a62bcbd22bfded74b93ccbc
thelonius182 <lcavdakker@gmail.com>
2020-01-30 23:38:02 +0100

    initial commit

---
+++ src/main.R
@@ -0,0 +1,31 @@
+# = = = = = = = = =
+# Check vulling van de wekelijkse playlists
+# = = = = = = = = =
+
+# - - - - - - - - -
+# Init
+# - - - - - - - - -
+library(tidyr)
+library(knitr)
+library(rmarkdown)
+library(googlesheets)
+library(RCurl)
+library(yaml)
+library(magrittr)
+library(stringr)
+library(dplyr)
+library(purrr)
+library(lubridate)
+library(fs)
+library(readr)
+library(futile.logger)
+library(keyring)
+library(RMySQL)
+library(officer)
+
+filter <- dplyr::filter # voorkom verwarring met stats::filter
+
+flog.appender(appender.file("/Users/scanner/Logs/ipl_scanner.log"), name = "ipls_log")
+flog.info("= = = = = iTunes Playlist Scanner start = = = = =", name = "ipls_log")
+
+config <- read_yaml("config.yaml")
---
+++ src/stage_host_reports.R
@@ -0,0 +1,7 @@
+iTunes_Playlist_Scanner_UitzendMac_30_01_20 <-
+  read_delim(
+    "//uitzendmac-cz/tech/Documents/Salsa/IPS-output/iTunes Playlist Scanner (UitzendMac), 30-01-20.txt",
+    "\t",
+    escape_double = FALSE,
+    trim_ws = TRUE
+  )
